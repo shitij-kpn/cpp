decf22e0704338772f55261f0bc02d97cec362d6
shitij_kpn <38817435+shitij-kpn@users.noreply.github.com>
2020-08-17 03:37:55 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,12 +15,7 @@ using namespace std;
 
 
 int main(){
-    #ifndef ONLINE_JUDGE
-        // for getting input from input.txt
-        freopen("input.txt", "r", stdin);
-        // for writing output to output.txt
-        freopen("output.txt", "w", stdout);
-    #endif 
+    
    	
     return 0;
 }
