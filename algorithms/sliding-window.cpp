f8388a7903bea00fa63bddb0a75dd83216c77369
shitij <shitijtalan@gmail.com>
2020-08-24 12:58:36 +0530

    sliding-window

---
+++ algorithms/sliding-window.cpp
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+//brute force (TLE)
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len = 0,maxlen = INT_MIN;
+        bool space = false;
+        if(s.empty()){
+            return 0;
+        }
+        for(int i=0;i<s.size();i++){
+            map<char,int> m;
+            len=0;
+            space = false;
+            for(int j=i;j<s.size();j++){
+                if(isspace(s[j])){
+                    if(space){
+                        break;
+                    }
+                    space = true;
+                    len++;   
+                }
+                else{
+                    if(m[s[j]] > 0){
+                        break;
+                    }
+                    len++;
+                    m[s[j]]++;
+                }
+            }
+            maxlen = max(maxlen,len);
+        }
+        return maxlen;
+    }
+};
+
+
+
+//optimised version(sliding window)
+
+
