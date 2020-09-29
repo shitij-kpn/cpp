0d1ccf18336d57bb50813425226fb020a72a5e98
shitij <shitijtalan@gmail.com>
2020-09-29 22:30:34 +0530

    672-div2

---
+++ contests/674-div3/a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios::sync_with_stdio(0), cout.tie()
+#define test long int T; cin>>T; while(T--)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll> 
+#define mui unordered_map<int,int>
+#define sui unordered_set<int>
+#define ml map<int,int>
+#define sl set<int>
+#define sz(a) int((a).size())
+#define MOD 1000000007
+#define mp make_pair
+#define pb push_back
+#define all(x) x.begin(),x.end()
+
+int main(){
+    test{
+        int n,x;
+        cin>>n>>x;
+        if(n<3){
+            cout<<1<<endl;
+            continue;
+        }
+        int ans = 1;
+        while(n>2){
+            n-=x;
+            ans++;
+        } 
+        cout<<ans<<endl;
+    }
+    return 0;
+}
