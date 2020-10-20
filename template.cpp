a7be747b63beb6b440b4ae34817601d9bd9cfb41
shitij <shitijtalan@gmail.com>
2020-10-20 17:54:12 +0530

    directory-changes

--- template.cpp
+++ template.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
-
+    
 using namespace std;
-
+    
 #define fast ios::sync_with_stdio(0), cout.tie()
 #define test long int T; cin>>T; while(T--)
 #define ll long long
 #define vi vector<int>
 #define vl vector<ll> 
-#define mint map<int,int>
+#define mui unordered_map<int,int>
+#define sui unordered_set<int>
+#define ml map<int,int>
+#define sl set<int>
 #define sz(a) int((a).size())
 #define MOD 1000000007
 #define mp make_pair
 #define pb push_back
-
-
+#define all(x) x.begin(),x.end()
+    
 int main(){
     test{
-        for(int i=0;i<5;i++){
-            cout<<i<<" ";
-        }
-        cout<<endl;
+        
     }
     return 0;
 }
