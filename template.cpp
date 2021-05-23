a124a77d6c3dcbff97bfd9118579cbb22976c3d2
shitij <shitijtalan@gmail.com>
2021-05-23 18:58:58 +0530

    greedy

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios::sync_with_stdio(0), cout.tie()
+#define test long int T; cin>>T; while(T--)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll> 
+#define mint map<int,int>
+#define sz(a) int((a).size())
+#define MOD 1000000007
+#define mp make_pair
+#define pb push_back
+
+
+int main(){
+    
+   	
+    return 0;
+}
+
